e10b02dc85bb464283486f9374f8538bfd20e439
Rotem Fogel <rotem@seekingalpha.com>
2022-10-30 16:07:44 +0200

    Foldables

--- src/main/scala/typeclasses/Folding.scala
+++ src/main/scala/typeclasses/Folding.scala
@@ -2,9 +2,10 @@ package typeclasses
 
 import cats.{Eval, Monoid}
 
+//noinspection DuplicatedCode
 object Folding {
 
-  object ListExcersizes {
+  object ListSupersizes {
     // TODO: Implement the following methods using foldLeft/foldRight
     def map[A, B](list: List[A])(f: A => B): List[B] =
       list.foldRight(List.empty[B])((a, b) => f(a) :: b)
@@ -23,19 +24,29 @@ object Folding {
   // Foldable is very good for generalizing fold operations on different types
   import cats.Foldable
   import cats.instances.list._ // implicit Foldable[List]
-  val sum = Foldable[List].foldLeft(list, 0)(_ + _) // 55
+  val foldableList = Foldable[List]
+  val sum = foldableList.foldLeft(list, 0)(_ + _) // 55
   import cats.instances.option._ // implicit Foldable[Option]
   val optionSum: Int = Foldable[Option].foldLeft(Option(2), 40)(_ + _) // 42
   // foldRight can be implemented in stack recursive way
   // using Eval will make it stack safe, since chaining Evals is tail recursive
-  val sumRight: Eval[Int] = Foldable[List].foldRight(list, Eval.now(0))((n, e) => e.map(_ + n)) // Eval(55)
+  val sumRight: Eval[Int] = foldableList.foldRight(list, Eval.now(0))((n, e) => e.map(_ + n)) // Eval(55)
   // using monoid
   import cats.instances.int._ // implicit Monoid[Int]
-  val monoidSum: Int = Foldable[List].combineAll(list)
-  val mappedConcat: String = Foldable[List].foldMap(list)(_.toString) // requires implicit Monoid[String]
+  val monoidSum: Int = foldableList.combineAll(list)
+  val mappedConcat: String = foldableList.foldMap(list)(_.toString) // requires implicit Monoid[String]
+
+  import cats.instances.vector._ // implicit Foldable[Vector]
+  val nested = List(Vector(1,2,3), Vector(4,5,6))
+  val nestedResult: Int = foldableList.compose(Foldable[Vector]).combineAll(nested)
+  import cats.syntax.foldable._
+
+  // extension methods
+  val sum3 = List(1,2,3).combineAll // requires implicit Foldable[List] and Monoid[Int]
+  val mappedConcat2: String = List(1,2,3).foldMap(_.toString)
 
   def main(args: Array[String]): Unit = {
-    import ListExcersizes._
+    import ListSupersizes._
     println(map(list)(_ + 1))
     println(flatMap(list)(a => if (a % 2 == 0) List(-a) else List.empty[Int]))
     println(filter(list)(_ %2 == 0))
@@ -47,5 +58,9 @@ object Folding {
     println("-" * 30)
     println(monoidSum)
     println(mappedConcat)
+    println("-" * 30)
+    println(nestedResult)
+    println(sum3)
+    println(mappedConcat2)
   }
 }
